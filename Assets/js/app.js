fe7b2346908a876b9bfa0d98f8bf924c748225a2
EJL <EvanJLong11@gmail.com>
2018-10-04 13:13:09 -0500

    google places api key working

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,5 +1,9 @@
 // api key
 const key = 'LBXLGGGLHCVTA6MUI2PB';
+// const placesKey = 'AIzaSyDW1KtYBTG9atRt_8QhxWi2GhO6uSBrzs0';
+// const placesKey2 = 'AIzaSyCWWEVzZHQvwp5dhHJMTaDRVnATsYWKGXY';
+// const placesKey3 = 'AIzaSyDliDoQC8OLbasGSdT_p5C55583bJ4q1eo';
+const placesKeypaid = 'AIzaSyCSYp0PROxz6148kSPkdUSJZj61kwy3Quo'
 
 //////////////this populates the category array//////////////
 categoryArray = [];
@@ -57,9 +61,9 @@ $.ajax({
 let eventArray = [];
 const city = $('#cityForm').val();
 // taking the value entered into the city field and storing it//
-const catID = $('#catForm').val();
+const catID = $('#inputState').val();
 
-console.log(city);
+
 
 // let dateinput = prompt("what date?");
 // let date = ("start_date.range_start=2018-10-05")
@@ -81,16 +85,39 @@ $.ajax({
       }
       console.log(eventArray);
       $('#id').append(`<div class="class"></div>`)
+      console.log(city);
+    console.log(catID);
   });
 }
  
 //   table.setData("`https://www.eventbriteapi.com/v3/events/search/?location.address=Dallas&expand=organizer,venue&token=LBXLGGGLHCVTA6MUI2PB`,", {key1:"name"}, "GET");
-
-
 
 function dropdownFunction() {
     var x = document.getElementById("mySelect").value;
 
 }
 
+
+$.ajax({
+    // url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurants&keyword=cruise&key=AIzaSyC3dwU3xZnvNdj1ZCr4r98XszdB3uu3N1o`,
+
+    // url: ` https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mongolian%20grill&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@47.6918452,-122.2226413&key=AIzaSyC3dwU3xZnvNdj1ZCr4r98XszdB3uu3N1o`,
+
+    // url: `https://maps.googleapis.com/maps/api/place/findplacefromtext/output?parameters`
+
+    url: `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=hotel&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:4000@47.6918452,-122.2226413&key=AIzaSyCSYp0PROxz6148kSPkdUSJZj61kwy3Quo`,
+    
+    method: 'GET'
+  }).then(function(res) {
+    //   for(var i = 0; i < res.events.length; i++){
+        // console.log(typeof(res.events[i].category_id));
+        console.log(res);
+    //    console.log(res.events[i].start.ti
+  });
+
+
+
+
 $('#searchBtn').on('click', searchFunction);
+
+
