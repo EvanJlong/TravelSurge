551f4dbe703cdf44ecc5c2a9cfa73d91224d7c99
EJL <EvanJLong11@gmail.com>
2018-10-03 19:20:41 -0500

    trying to get the dropdown selectable

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,5 +1,7 @@
+// api key
 const key = 'LBXLGGGLHCVTA6MUI2PB';
 
+//////////////this populates the category array//////////////
 categoryArray = [];
 $.ajax({
     url: `https://www.eventbriteapi.com/v3/categories/?token=${key}` ,
@@ -19,10 +21,13 @@ $.ajax({
         categoryArray.push(categories);
     }
     for (let i = 0; i < categoryArray.length; i++){
-        $(".dropdown-menu").append(`<a class = "dropdown-item">${categoryArray[i].name}</a>`)
+        $(".dropdown-menu").append(`<a class="dropdown-item">${categoryArray[i].name}</a>`)
       } 
-    
+    //   <a class="dropdown-item" href="#">${categoryArray[i].name}</a>
   });
+//////////////category array has now been populated and appended to the dropdown//////////////
+    // taking the value entered into the city field and storing it//
+
 
 //   console.log(categoryArray);
 
@@ -47,32 +52,38 @@ $.ajax({
 
 
 //CATEGORIES// //CATEGORIES// //CATEGORIES// //CATEGORIES// 
-// const city = $('#citField').val();
-// const catID = $('#catField').val();
+let eventArray = [];
+const city = $('#cityForm').val();
+// taking the value entered into the city field and storing it//
+const catID = $('#catForm').val();
 
-// // let dateinput = prompt("what date?");
-// // let date = ("start_date.range_start=2018-10-05")
-// let eventArray = [];
-// $.ajax({
-//     url: `https://www.eventbriteapi.com/v3/events/search/?location.address=${city}&expand=organizer,venue&token=${key}`,
-//     // Input the method type here (Hint: 'GET', 'POST', 'PUT', 'DELETE')
-//     method: 'GET'
-//   }).then(function(res) {
-//       for(var i = 0; i < res.events.length; i++){
-//         // console.log(typeof(res.events[i].category_id));
-//         // console.log(res);
-//     //    console.log(res.events[i].start.timezone);
-//     if (`${catID}` === res.events[i].category_id){
-//         eventArray.push(res.events[i])
+console.log(city);
+
+// let dateinput = prompt("what date?");
+// let date = ("start_date.range_start=2018-10-05")
+
+const searchFunction = function(){
+$.ajax({
+    url: `https://www.eventbriteapi.com/v3/events/search/?location.address=${city}&expand=organizer,venue&token=${key}`,
+    // Input the method type here (Hint: 'GET', 'POST', 'PUT', 'DELETE')
+    method: 'GET'
+  }).then(function(res) {
+      for(var i = 0; i < res.events.length; i++){
+        // console.log(typeof(res.events[i].category_id));
+        // console.log(res);
+    //    console.log(res.events[i].start.timezone);
+    if (`${catID}` === res.events[i].category_id){
+        eventArray.push(res.events[i])
        
-//     }
-//       }
-//       console.log(eventArray);
-//       $('#id').append(`<div class="class"></div>`)
-//   });
-
+    }
+      }
+      console.log(eventArray);
+      $('#id').append(`<div class="class"></div>`)
+  });
+}
  
 //   table.setData("`https://www.eventbriteapi.com/v3/events/search/?location.address=Dallas&expand=organizer,venue&token=LBXLGGGLHCVTA6MUI2PB`,", {key1:"name"}, "GET");
 
 
 
+$('#searchBtn').on('click', searchFunction);
