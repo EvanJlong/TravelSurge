f1220379bab28668b981b119e3b994e9919c9603
EJL <EvanJLong11@gmail.com>
2018-10-04 15:04:02 -0500

    categories and bars searching properly

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -60,19 +60,22 @@ $.ajax({
 
 //CATEGORIES// //CATEGORIES// //CATEGORIES// //CATEGORIES// 
 let eventArray = [];
-const city = $('#cityForm').val();
-// taking the value entered into the city field and storing it//
-const catID = $('#inputState').val();
-
 
 
 // let dateinput = prompt("what date?");
 // let date = ("start_date.range_start=2018-10-05")
 
-const searchFunction = function(){
+const searchFunction = function(e){
+    e.preventDefault();
+    const city = $('#cityForm').val().trim();
+    // let city = prompt("asdf");
+
+// taking the value entered into the city field and storing it//
+    const catID = $('#inputState').val();
 $.ajax({
     url: `https://www.eventbriteapi.com/v3/events/search/?location.address=${city}&expand=organizer,venue&token=${key}`,
-    // Input the method type here (Hint: 'GET', 'POST', 'PUT', 'DELETE')
+
+
     method: 'GET'
   }).then(function(res) {
       for(var i = 0; i < res.events.length; i++){
@@ -81,40 +84,48 @@ $.ajax({
     //    console.log(res.events[i].start.timezone);
     if (`${catID}` === res.events[i].category_id){
         eventArray.push(res.events[i])
-       
+        
     }
+
       }
       console.log(eventArray);
-      $('#id').append(`<div class="class"></div>`)
+      
+    //   $('#id').append(`<div class="class"></div>`)
       console.log(city);
     console.log(catID);
+
+    $('#cityForm').empty();
+    $("#inputState").val();
+    eventArray = [];
+    // $("select#inputState").change(resetFieldToDefault);
+
   });
 }
  
 //   table.setData("`https://www.eventbriteapi.com/v3/events/search/?location.address=Dallas&expand=organizer,venue&token=LBXLGGGLHCVTA6MUI2PB`,", {key1:"name"}, "GET");
 
-function dropdownFunction() {
-    var x = document.getElementById("mySelect").value;
+// function dropdownFunction() {
+//     var x = document.getElementById("mySelect").value;
 
-}
+// }
 
 
-$.ajax({
-    // url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurants&keyword=cruise&key=AIzaSyC3dwU3xZnvNdj1ZCr4r98XszdB3uu3N1o`,
+// $.ajax({
+//     // url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurants&keyword=cruise&key=AIzaSyC3dwU3xZnvNdj1ZCr4r98XszdB3uu3N1o`,
 
-    // url: ` https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mongolian%20grill&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@47.6918452,-122.2226413&key=AIzaSyC3dwU3xZnvNdj1ZCr4r98XszdB3uu3N1o`,
+//     // url: ` https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mongolian%20grill&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@47.6918452,-122.2226413&key=AIzaSyC3dwU3xZnvNdj1ZCr4r98XszdB3uu3N1o`,
 
-    // url: `https://maps.googleapis.com/maps/api/place/findplacefromtext/output?parameters`
+//     // url: `https://maps.googleapis.com/maps/api/place/findplacefromtext/output?parameters`
 
-    url: `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=hotel&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:4000@47.6918452,-122.2226413&key=AIzaSyCSYp0PROxz6148kSPkdUSJZj61kwy3Quo`,
+//     url: `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=hotel&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:4000@47.6918452,-122.2226413&key=AIzaSyCSYp0PROxz6148kSPkdUSJZj61kwy3Quo`,
     
-    method: 'GET'
-  }).then(function(res) {
-    //   for(var i = 0; i < res.events.length; i++){
-        // console.log(typeof(res.events[i].category_id));
-        console.log(res);
-    //    console.log(res.events[i].start.ti
-  });
+//     method: 'GET'
+//   }).then(function(res) {
+//     //   for(var i = 0; i < res.events.length; i++){
+//         // console.log(typeof(res.events[i].category_id));
+//         console.log(res);
+//     //    console.log(res.events[i].start.ti
+//   });
 
 
 
